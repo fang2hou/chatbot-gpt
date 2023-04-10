7708d5071f55f23b180cffebe509671bd93e45da
Zhou Fang <fang2hou@outlook.com>
2023-04-11 01:33:58 +0900

    refactor: use async processing in release script

--- scripts/discord_release_notification.ts
+++ scripts/discord_release_notification.ts
@@ -1,49 +1,48 @@
-import {readFile} from 'fs';
+import {readFile} from 'fs/promises';
 import {APIEmbedField, EmbedAuthorOptions, EmbedBuilder, RestOrArray, WebhookClient} from 'discord.js';
+import {Configuration, OpenAIApi} from "openai";
 
-const {Configuration, OpenAIApi} = require("openai");
-
-const REPOSITORY: string = process.env.GITHUB_REPOSITORY || '';
-const TAG: string = process.env.GITHUB_REF_NAME || '';
-const GITHUB_URL: string = process.env.GITHUB_SERVER_URL || '';
-const DISCORD_WEBHOOK_URL: string = process.env.DISCORD_WEBHOOK_URL || '';
-const OPENAI_API_KEY: string = process.env.OPENAI_API_KEY || '';
-
-if (REPOSITORY.length === 0) {
-    console.error('GITHUB_REPOSITORY is not set');
-    process.exit(1);
+// env returns the value of the environment variable with the given name.
+let env = (envName: string): string => {
+    let value = process.env[envName];
+    if (value == null || value.length === 0) {
+        throw new Error(`Environment variable ${envName} is not set`);
+    }
+    return value;
 }
 
-if (TAG.length === 0) {
-    console.error('GITHUB_REF_NAME is not set');
-    process.exit(1);
-}
+// getArtifactUrl returns the URL of the artifact with the given name.
+const getArtifactUrl = (prefix: string, artifactName: string): string => {
+    return `${artifactsUrlBase}/${prefix}_${artifactName}`;
+};
 
-if (GITHUB_URL.length === 0) {
-    console.error('GITHUB_SERVER_URL is not set');
-    process.exit(1);
-}
+// artifactSuffixes is a map of artifact names to their suffixes.
+const artifactSuffixes: { [key: string]: string } = {
+    'macOS 英特尔芯片': 'darwin_amd64',
+    'macOS Apple 芯片': 'darwin_arm64',
+    'Linux AMD 64 位': 'linux_amd64',
+    'Linux ARM 64 位': 'linux_arm64',
+    'Linux ARMv6 芯片': 'linux_armv6',
+    'Linux ARMv7 芯片': 'linux_armv7',
+    'Windows AMD 64 位': 'windows_amd64.exe',
+    'Windows ARM 64 位': 'windows_arm64.exe',
+};
 
-if (DISCORD_WEBHOOK_URL.length === 0) {
-    console.error('DISCORD_WEBHOOK_URL is not set');
-    process.exit(1);
-}
+const REPOSITORY: string = env('GITHUB_REPOSITORY');
+const TAG: string = env('GITHUB_REF_NAME');
+const GITHUB_URL: string = env('GITHUB_SERVER_URL');
+const DISCORD_WEBHOOK_URL: string = env('DISCORD_WEBHOOK_URL');
+const OPENAI_API_KEY: string = env('OPENAI_API_KEY');
 
 const projectName: string = REPOSITORY.split('/')[1];
 const releaseUrl: string = `${GITHUB_URL}/${REPOSITORY}/releases/tag/${TAG}`;
 const artifactsUrlBase: string = `${GITHUB_URL}/${REPOSITORY}/releases/download/${TAG}`;
 
-readFile('./changes.md', 'utf8', (err: NodeJS.ErrnoException | null, data: string) => {
-    if (err) {
-        console.error(`Error reading changes.md: ${err}`);
-        return;
-    }
-
-    const fields: RestOrArray<APIEmbedField> = [];
-
+async function main() {
+    let changes: string = await readFile('./changes.md', 'utf8');
     let releaseNote: string = '';
-    // Create the formatted message content
-    data.split('\n').forEach((line: string) => {
+
+    changes.split('\n').forEach((line: string) => {
         if (line.startsWith('build')) {
             releaseNote += `• :tools: ${line.trim()}\n`;
         } else if (line.startsWith('feat')) {
@@ -76,52 +75,35 @@ readFile('./changes.md', 'utf8', (err: NodeJS.ErrnoException | null, data: strin
         }
     });
 
+    // Translate the release note to Chinese
+    let openaiClient = new OpenAIApi(new Configuration({apiKey: OPENAI_API_KEY}))
+    let translationResp = await openaiClient.createChatCompletion({
+        model: "gpt-3.5-turbo-0301",
+        messages: [
+            {
+                role: "user",
+                content: "You are a translator for translating the software changelog to chinese," +
+                    "your answer should not include anything outside the list." +
+                    "Do not translate the word matched ':.*:', just keep it as it is."
+            },
+            {
+                role: "user",
+                content: releaseNote
+            },
+        ],
+    });
 
-    if (OPENAI_API_KEY.length !== 0) {
-        const openai = new OpenAIApi(new Configuration({
-            apiKey: OPENAI_API_KEY,
-        }));
-
-        openai.createChatCompletion({
-            model: "gpt-3.5-turbo-0301",
-            messages: [
-                {
-                    role: "user",
-                    content: "You are a translator for translating the software changelog to chinese," +
-                        "your answer should not include anything outside the list." +
-                        "Do not translate the word 'token' and ':.*:', just keep it as it is."
-                },
-                {
-                    role: "user",
-                    content: releaseNote
-                },
-            ],
-        }).then((response: any) => {
-            releaseNote = response.data.choices[0].message;
-        }).catch((err: any) => {
-            console.error(err);
-        });
+    if (translationResp.data.choices[0].message != null){
+        releaseNote = translationResp.data.choices[0].message.content;
+        releaseNote += '\n\n:beginner: 以上内容由 OpenAI GPT-3.5 Turbo 生成';
     }
+
+    const fields: RestOrArray<APIEmbedField> = [];
 
     fields.push(
         {name: '**变更列表**', value: releaseNote, inline: false},
         {name: '\u200B', value: '\u200B', inline: false}
     );
-
-    const getArtifactUrl = (prefix: string, artifactName: string): string => {
-        return `${artifactsUrlBase}/${prefix}_${artifactName}`;
-    };
-
-    const artifactSuffixes: { [key: string]: string } = {
-        'macOS 英特尔芯片': 'darwin_amd64',
-        'macOS Apple 芯片': 'darwin_arm64',
-        'Linux AMD 64 位': 'linux_amd64',
-        'Linux ARM 64 位': 'linux_arm64',
-        'Linux ARMv6 芯片': 'linux_armv6',
-        'Linux ARMv7 芯片': 'linux_armv7',
-        'Windows AMD 64 位': 'windows_amd64.exe',
-        'Windows ARM 64 位': 'windows_arm64.exe',
-    };
 
     for (const [key, value] of Object.entries(artifactSuffixes)) {
         fields.push({
@@ -148,15 +130,11 @@ readFile('./changes.md', 'utf8', (err: NodeJS.ErrnoException | null, data: strin
         url: DISCORD_WEBHOOK_URL,
     });
 
-    webhookClient
-        .send({embeds: [embed]})
-        .then(() => {
-            console.log('Discord notification sent successfully!');
-        })
-        .catch((error: Error) => {
-            console.error('Failed to send Discord notification:', error);
-        })
-        .finally(() => {
-            webhookClient.destroy();
-        });
+    await webhookClient.send({embeds: [embed]}).catch((error) => {console.error(error)});
+    webhookClient.destroy();
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exit(1);
 });
